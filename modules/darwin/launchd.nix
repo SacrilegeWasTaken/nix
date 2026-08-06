ac1b989bb8b7f6eb556edc326d6a694464ed50af
Leonid <superdjskater@mail.ru>
2026-08-06 04:42:33 +0300

    feat(launchd): autostart bazel-remote with a 50 GiB cache

--- modules/darwin/launchd.nix
+++ modules/darwin/launchd.nix
@@ -1,5 +1,8 @@
 { config, pkgs, username, ... }:
 
+let
+  bazelRemoteDir = "/Users/${username}/.cache/bazel-remote";
+in
 {
   launchd.user.agents = {
     "com.${username}.raycast" = {
@@ -22,6 +25,24 @@
       serviceConfig.KeepAlive = false;
       serviceConfig.StandardOutPath = "/tmp/com.${username}.olovebar.out.log";
       serviceConfig.StandardErrorPath = "/tmp/com.${username}.olovebar.err.log";
+    };
+    # Local REAPI cache for buck2/bazel builds (Homebrew formula, see homebrew.nix).
+    # bazel-remote refuses to start without an existing --dir, and it has no flag
+    # to turn the HTTP listener off, so both are handled here: the cache dir is
+    # created on start and HTTP is pinned to loopback next to the gRPC port.
+    "com.${username}.bazel-remote" = {
+      script = ''
+        /bin/mkdir -p ${bazelRemoteDir}
+        exec /opt/homebrew/bin/bazel-remote \
+          --dir ${bazelRemoteDir} \
+          --max_size 50 \
+          --http_address localhost:9091 \
+          --grpc_address localhost:9092
+      '';
+      serviceConfig.RunAtLoad = true;
+      serviceConfig.KeepAlive = true;
+      serviceConfig.StandardOutPath = "/tmp/com.${username}.bazel-remote.out.log";
+      serviceConfig.StandardErrorPath = "/tmp/com.${username}.bazel-remote.err.log";
     };
   };
 }
